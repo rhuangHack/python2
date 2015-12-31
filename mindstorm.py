688be5813aebc60df67ad8c64ed4d44c8a9594c8
albert huang <hrd000621@hotmail.com>
2015-12-30 20:15:53 -0800

    add instance to draw triangle and seperate functions

--- mindstorm.py
+++ mindstorm.py
@@ -1,29 +1,42 @@
 import turtle
 
-def draw_spuare():
+def draw_square(some_turtle):
+    for i in range(0, 4):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+        i = i + 1
+
+
+def draw_triangle(some_turtle):
+    for index in range(1, 4):
+        some_turtle.forward(100)
+        some_turtle.left(120)
+        index = index + 1
+
+def draw_art():
+    #set background
     window = turtle.Screen()
     window.bgcolor("red")
 
-
+    #set square turtle
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("black")
     brad.speed(1)
-
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-
+    draw_square(brad)
+    
+    #set and draw circle
     angle = turtle.Turtle()
     angle.shape("arrow")
     angle.color("blue")
     angle.circle(100)
 
-    window.exitonclick()
+    #set and draw triangle
+    tri = turtle.Turtle()
+    tri.shape("circle")
+    tri.color("white")
+    draw_triangle(tri)
 
-draw_spuare()
+
+    window.exitonclick()
+draw_art()
