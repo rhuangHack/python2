78bd07c1fdea1c0fe3b858c43f8c7cf0b0ca3ea5
albert huang <hrd000621@hotmail.com>
2015-12-30 21:43:41 -0800

    bonus part, use spade to draw a flower

--- mindstorm.py
+++ mindstorm.py
@@ -10,7 +10,15 @@ def draw_triangle(some_turtle):
     for index in range(1, 4):
         some_turtle.forward(100)
         some_turtle.left(120)
-        index = index + 1
+
+def draw_spade(some_turtle):
+    some_turtle.forward(120)
+    some_turtle.right(60)
+    some_turtle.forward(120)
+    some_turtle.right(120)
+    some_turtle.forward(120)
+    some_turtle.right(60)
+    some_turtle.forward(120)
 
 def draw_art():
     #set background
@@ -18,13 +26,24 @@ def draw_art():
     window.bgcolor("white")
 
     #set square turtle
-    brad = turtle.Turtle()
-    brad.shape("turtle")
-    brad.color("black")
-    brad.speed(2)
+    #brad = turtle.Turtle()
+    #brad.shape("turtle")
+    #brad.color("black")
+    #brad.speed(2)
+    #for i in range(1, 37):
+    #    draw_square(brad)
+    #    brad.right(10)
+
+    #set bonus part graph
+    bonus = turtle.Turtle()
+    bonus.shape("turtle")
+    bonus.color("black")
+    bonus.speed(8)
     for i in range(1, 37):
-        draw_square(brad)
-        brad.right(10)
+        draw_spade(bonus)
+        bonus.right(10)
+    bonus.right(90)
+    bonus.forward(250)
     
     #set and draw circle
     #angle = turtle.Turtle()
