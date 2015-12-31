480cf6c7ba28f1a78514a0ab640f913530cfda22
albert huang <hrd000621@hotmail.com>
2015-12-30 23:45:54 -0800

    refine the function of spade drawing

--- mindstorm.py
+++ mindstorm.py
@@ -11,14 +11,19 @@ def draw_triangle(some_turtle):
         some_turtle.forward(100)
         some_turtle.left(120)
 
-def draw_spade(some_turtle):
-    some_turtle.forward(120)
-    some_turtle.right(60)
-    some_turtle.forward(120)
-    some_turtle.right(120)
-    some_turtle.forward(120)
-    some_turtle.right(60)
-    some_turtle.forward(120)
+def draw_spade(some_turtle, length, angle):
+    for i in range(1,3):
+        some_turtle.forward(length)
+        some_turtle.right(180 - angle)
+        some_turtle.forward(length)
+        some_turtle.right(angle)
+    #some_turtle.forward(120)
+    #some_turtle.right(60)
+    #some_turtle.forward(120)
+    #some_turtle.right(120)
+    #some_turtle.forward(120)
+    #some_turtle.right(60)
+    #some_turtle.forward(120)
 
 def draw_art():
     #set background
@@ -38,12 +43,18 @@ def draw_art():
     bonus = turtle.Turtle()
     bonus.shape("turtle")
     bonus.color("black")
-    bonus.speed(8)
-    for i in range(1, 37):
-        draw_spade(bonus)
-        bonus.right(10)
+    bonus.speed(0)
+    for i in range(1, 73):
+        draw_spade(bonus, 150, 140)
+        bonus.right(5)
     bonus.right(90)
-    bonus.forward(250)
+    bonus.forward(350)
+
+    bonus.color("green")
+    bonus.right(80)
+    draw_spade(bonus, 150, 140)
+    bonus.right(160)
+    draw_spade(bonus, 150, 140)
     
     #set and draw circle
     #angle = turtle.Turtle()
