2943182ea070a3f09a337af7198f4706c2937b28
albert huang <hrd000621@hotmail.com>
2015-12-30 20:42:07 -0800

    make turtle draw a around of squares

--- mindstorm.py
+++ mindstorm.py
@@ -4,7 +4,6 @@ def draw_square(some_turtle):
     for i in range(0, 4):
         some_turtle.forward(100)
         some_turtle.right(90)
-        i = i + 1
 
 
 def draw_triangle(some_turtle):
@@ -16,26 +15,28 @@ def draw_triangle(some_turtle):
 def draw_art():
     #set background
     window = turtle.Screen()
-    window.bgcolor("red")
+    window.bgcolor("white")
 
     #set square turtle
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("black")
-    brad.speed(1)
-    draw_square(brad)
+    brad.speed(2)
+    for i in range(1, 37):
+        draw_square(brad)
+        brad.right(10)
     
     #set and draw circle
-    angle = turtle.Turtle()
-    angle.shape("arrow")
-    angle.color("blue")
-    angle.circle(100)
+    #angle = turtle.Turtle()
+    #angle.shape("arrow")
+    #angle.color("blue")
+    #angle.circle(100)
 
     #set and draw triangle
-    tri = turtle.Turtle()
-    tri.shape("circle")
-    tri.color("white")
-    draw_triangle(tri)
+    #tri = turtle.Turtle()
+    #tri.shape("circle")
+    #tri.color("white")
+    #draw_triangle(tri)
 
 
     window.exitonclick()
